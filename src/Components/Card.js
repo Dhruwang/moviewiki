62cc55ab378e762030187c98c9255c37b0c30779
Dhruwang <dhruwangjariwala18@gmail.com>
2022-12-26 17:17:19 +0530

    db created for fav

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,13 +1,32 @@
-import React,{useState,useRef} from 'react'
+import React,{useState,useRef,useEffect} from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Card(props) {
     const like = useRef()
-    const [favourites, setfavourites] = useState([])
-    const handleLike=(id)=>{
-        setfavourites(favourites.push(id))
-        console.log(favourites)
+    const host = 'http://localhost:5000'
+
+
+
+    const handleLike= async (id)=>{
+        console.log("submitted")
+        const response = await fetch(`${host}/api/auth/addFav`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+              'auth-token' : localStorage.getItem('token')
+            },
+            body: JSON.stringify({movieId:id})
+    
+          });
+          const json = await response.json()
+          console.log(json)
+          if(json.success){
+            // save authToken and redirect 
+            console.log("added")
+          }
     }
+
+ 
   return (
     <div>
         <div className='Trendcard' key={props.index} style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w342${props.poster_path})` }}>
